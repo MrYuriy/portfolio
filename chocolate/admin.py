78d0cf310752e8ec47085ffd81ff4aeff5b35338
MrYuriy <yuriybabiyk@gmail.com>
2024-04-16 17:19:23 +0200

    Add model Project and register to admin

--- chocolate/admin.py
+++ chocolate/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+class ProlectAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+admin.site.register(Project ,ProlectAdmin)
--- chocolate/models.py
+++ chocolate/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    home_link = models.CharField(max_length=200)
+    short_description = models.TextField()
+    long_deskription = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
